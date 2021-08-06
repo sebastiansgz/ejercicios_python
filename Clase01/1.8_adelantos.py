a44500b1a276dffdd40844ffc9328a52414770b9
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-06 19:59:31 -0300

    Ejercicio 1.9 resuelto

--- Clase01/1.8_adelantos.py
+++ Clase01/1.8_adelantos.py
@@ -6,17 +6,15 @@ saldo = 500000.0
 tasa = 0.05
 pago_mensual = 2684.11
 total_pagado = 0.0
-meses = 0
+mes = 0
 
 while saldo > 0:
-    meses += 1
-    if meses <= 12:
+    mes += 1
+    if mes <= 12:
         saldo = saldo * (1+tasa/12) - pago_mensual - 1000
         total_pagado += pago_mensual + 1000
     else:
         saldo = saldo * (1+tasa/12) - pago_mensual 
         total_pagado += pago_mensual      
       
-print(f'Total pagado: {round(total_pagado, 2)} en {meses}')  #Preguntar por el numero 1 o 2 
-
-## mensaje prueba commit 2
+print(f'Total pagado: {round(total_pagado, 2)} en {mes}')  #Preguntar por el numero 1 o 2 
---
+++ Clase01/1.9.py
@@ -0,0 +1,23 @@
+saldo = 500000.0
+tasa = 0.05
+pago_mensual = 2684.11
+total_pagado = 0.0
+mes = 0
+
+pago_extra_mes_comienzo = 61
+pago_extra_mes_fin = 108
+pago_extra = 1000
+
+while saldo > 0:
+    mes += 1
+    if mes >= pago_extra_mes_comienzo and mes <= pago_extra_mes_fin:
+        saldo = saldo * (1+tasa/12) - pago_mensual - pago_extra
+        total_pagado += pago_mensual + pago_extra 
+
+    else:
+        saldo = saldo * (1+tasa/12) - pago_mensual 
+        total_pagado += pago_mensual 
+    
+              
+      
+print(f'Total pagado: {round(total_pagado, 2)} en {mes}')
