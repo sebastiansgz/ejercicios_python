4c638a9788d6f95504b06ff979fa64b2ec5783dc
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-06 20:13:58 -0300

    Ejercicio 1.10

---
+++ Clase01/1.10.py
@@ -0,0 +1,24 @@
+saldo = 500000.0
+tasa = 0.05
+pago_mensual = 2684.11
+total_pagado = 0.0
+mes = 0
+
+pago_extra_mes_comienzo = 61
+pago_extra_mes_fin = 108
+pago_extra = 1000
+
+while saldo > 0:
+    mes += 1
+    if mes >= pago_extra_mes_comienzo and mes <= pago_extra_mes_fin:
+        saldo = saldo * (1+tasa/12) - pago_mensual - pago_extra
+        total_pagado += pago_mensual + pago_extra 
+        print(mes, round(total_pagado, 2), round(saldo,2))
+    else:
+        saldo = saldo * (1+tasa/12) - pago_mensual 
+        total_pagado += pago_mensual
+        print(mes, round(total_pagado,2), round(saldo,2))
+    
+              
+      
+print(f'Total pagado: {round(total_pagado, 2)} \nMeses: {mes}')
