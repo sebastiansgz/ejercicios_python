7cfdc6453a74f343a5995a12318a7c2cbc522b3c
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-07 00:53:58 -0300

    Geringoso

---
+++ Clase01/1.18.py
@@ -0,0 +1,15 @@
+cadena = 'Geringoso'
+
+capadepenapa = ''
+
+for c in cadena:
+      
+    if c in ('a', 'e', 'i', 'o', 'u'):
+           
+        capadepenapa = capadepenapa + c + 'p' + c
+                
+    else:
+        capadepenapa = capadepenapa + c
+    
+        
+print(capadepenapa)
