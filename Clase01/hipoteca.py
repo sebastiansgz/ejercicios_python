f6cb9570144d505758e27149d1a450254bdfb49e
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-07 02:51:52 -0300

    Limpieza
    Eliminé líneas innecesarias.

--- Clase01/hipoteca.py
+++ Clase01/hipoteca.py
@@ -13,18 +13,19 @@ while saldo > 0:
     if mes >= pago_extra_mes_comienzo and mes <= pago_extra_mes_fin:
         saldo = saldo * (1+tasa/12) - pago_mensual - pago_extra
         total_pagado += pago_mensual + pago_extra 
-        print(mes, round(total_pagado, 2), round(saldo,2))
+        
     
     else:
         if saldo < pago_mensual:
             total_pagado += saldo * (1+tasa/12)
             saldo = saldo - saldo
-            print(mes, round(total_pagado,2), round(saldo,2))
+            
 
         else:    
             saldo = saldo * (1+tasa/12) - pago_mensual 
             total_pagado += pago_mensual
-            print(mes, round(total_pagado,2), round(saldo,2))
+            
+    print(mes, round(total_pagado,2), round(saldo,2))        
         
     
               
