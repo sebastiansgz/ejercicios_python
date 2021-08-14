0640570c92d6a439eebf670b08f63b9bbbaa17cd
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-14 13:05:55 -0300

    Solucion 2.7
    Creacion de funcion a partir del ejercicio 2.3

---
+++ Clase02/2.7.py
@@ -0,0 +1,26 @@
+def buscar_precio(fruta):
+    is_there = False          
+    with open('../Data/precios.csv', 'rt') as file:            
+        for line in file:            
+            row = line.split(',')  
+            if str(row[0]).lower() == fruta.lower():
+                is_there = True
+                if is_there:
+                    precio_fruta = float(row[1])
+                print(f'El precio de la {fruta} es: {precio_fruta}')         
+        if is_there == False:
+            print(f'{fruta} no figura en el listado de precios')     
+                                              
+                      
+                 
+                
+
+
+                                      
+                 
+                
+    
+                
+    
+fruta = input('Ingrese la fruta para consultar su precio: ')
+precio = buscar_precio(fruta)
