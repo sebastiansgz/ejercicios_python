93f6ad0dc1ec2a2de86fa8a87e9aa13d6c6946d3
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-15 00:11:56 -0300

    Limpieza
    Arreglo del ejercicio costo_camion.py (ejercicio 2.6)

--- Clase02/costo_camion.py
+++ Clase02/costo_camion.py
@@ -1,9 +1,11 @@
+import csv
+
 def costo_camion(archivo):
     with open(archivo, 'rt') as file:
-        headers = next(file).split()
+        rows = csv.reader(file)
+        headers = next(rows)
         costo_total = float()
-        for line in file:
-            row = line.split(',')
+        for row in rows:
             costo_por_fruta = float(row[1]) * float(row[2])
             costo_total += costo_por_fruta
     return costo_total
