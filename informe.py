238008347ed3dcf53be8ae9e5dccb3c630a8b17f
seb <57051422+sebastiansgz@users.noreply.github.com>
2021-08-21 14:44:39 -0300

    Reecritura informe.py
    Definí una nueva función para hacer el calcula del balance. Ahora el código es más limpio

--- informe.py
+++ informe.py
@@ -6,36 +6,7 @@ En caso de no proveer la ubicacion de dichos archivos este script asume que se t
 import csv
 import sys
 
-camion = []
-precios = []
-
-def leer_camion(archivo_camion):
-    with open(archivo_camion, 'rt') as file:
-        rows = csv.reader(file)
-        headers = next(rows)        
-        for row in rows:
-            lote = {
-                headers[0] : row[0],
-                headers[1] : int(row[1]), 
-                headers[2] : float(row[2])
-            }
-            camion.append(lote)
-    return camion
-
-
-def leer_precios(archivo_precios):
-    with open(archivo_precios, 'rt') as file:        
-        rows = csv.reader(file)
-        for row in rows:
-            try:
-                nombre = row[0]            
-                precio = float(row[1])
-                par = (nombre , precio)
-                precios.append(par)                
-            except IndexError:
-                print(f'Alerta: En alguna/s linea/s del archivo {archivo_precios} no se encuentran los elementos necesarios para armar un par "key:value"')
-    return dict(precios)
-
+# Hago uso de sys.argv para tomar los argumentos que paso al momento de ejecutar el script desde la linea de comandos como el nombre de los archivos que van a ser utilizados.
 
 if len(sys.argv) == 3:
     archivo_camion = sys.argv[1] 
@@ -45,33 +16,64 @@ else:
     archivo_precios ='Data/precios.csv'
 
 
-camion = leer_camion(archivo_camion)  # Tengo una lsta que contiene diccionarios
+# ---------------------------------------------- Defino las funciones------------------------------------
+#Función para manejar el archivo del camion.
+def leer_camion(nombre_archivo):
+    camion = []                                        # Importante: Definir la lista vacia dentro de la función así inicia vacia siempre.
+    with open(nombre_archivo, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)        
+        for row in rows:
+            lote = dict(zip(headers, row))
+            lote['cajones'] = int(lote['cajones'])    
+            lote['precio'] = float(lote['precio'])    
+            camion.append(lote)            
+    return camion
+
+#Función para manejar el archivo de precios.
+def leer_precios(archivo_precios):
+    precios = []
+    with open(archivo_precios, 'rt') as file:        
+        rows = csv.reader(file)
+        for n_row, row in enumerate(rows, start= 1):
+            try:
+                nombre = row[0]            
+                precio = float(row[1])
+                par = (nombre , precio)
+                precios.append(par)                
+            except IndexError:
+                print(f'Alerta: En la linea {n_row} del archivo {archivo_precios} no se encuentran los elementos necesarios para armar un par "key:value"')
+    return dict(precios)
+
+# Función para calcular el balance. 
+
+def calcular_balance(camion, precios):    
+    costo_total = float()
+    venta_total = float()
+    for lote in camion:
+        costo_total += lote['cajones'] * lote['precio']
+        producto = lote['nombre']
+        venta_total += precios[producto] * lote['cajones']
+    balance = venta_total - costo_total
+    if balance >= 0:
+        print(f'BALANCE: \n Costo total: {costo_total} \n Total ventas: {venta_total}\nLa ganacia fue de: {balance}')
+    else:
+        print(f'BALANCE: \n Costo total: {costo_total} \n Total ventas: {venta_total}\nLas perdidas fueron de: {balance}')
+    return 
+
+# ------------------------------- Utilizo las funciones----------------------------------
+camion = leer_camion(archivo_camion)  # Tengo una lista que contiene diccionarios
 precios = leer_precios(archivo_precios) # Un diccionario con {'nombre' : precio,(...)}
-costo_total = float()
-venta_total = float()
 
-for lote in camion:
-    costo_total += lote['cajones'] * lote['precio']
-    producto = lote['nombre']
-    venta_total += precios[producto] * lote['cajones']    
-
-balance = venta_total - costo_total
-
-
-print(f'BALANCE: \n Costo total: {costo_total} \n Total ventas: {venta_total}')
-if balance > 0:
-    print(f'La ganacia fue de: {balance}')
-elif balance < 0:
-    print(f'Las perdidas fueron de: {balance}')
-
+calcular_balance(camion,precios)
 
 '''
 OUTPUT
 
-Alerta: En alguna/s linea/s del archivo Data/precios.csv no se encuentran los elementos necesarios para armar un par "key:value"
+Alerta: En la linea 31 del archivo Data/precios.csv no se encuentran los elementos necesarios para armar un par "key:value"
 BALANCE: 
  Costo total: 47671.15 
  Total ventas: 62986.1
-La ganacia fue de: 15314.949999999997
+La ganancia fue de: 15314.949999999997
 
 '''
